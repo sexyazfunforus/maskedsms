f8be3eb5f322ec01ad2d37b60e9af34282a6eff1
sexyazfunforus <sexyazfunforus@gmail.com>
2017-01-19 20:13:41 -0700

    version 1
    give it a shot

---
+++ anonymous-communications-rails/app/models/reservation.rb
@@ -0,0 +1,86 @@
+class Reservation < ActiveRecord::Base
+  validates :name, presence: true
+  validates :guest_phone, presence: true
+
+  enum status: [ :pending, :confirmed, :rejected ]
+
+  belongs_to :vacation_property
+  belongs_to :user
+
+  def notify_host(force = false)
+    # Don't send the message if we have more than one and we aren't being forced
+    if self.host.pending_reservations.length > 1 and !force
+      return
+    else
+      message = "You have a new reservation request from #{self.name} for #{self.vacation_property.description}:
+
+      '#{self.message}'
+
+      Reply [accept] or [reject]."
+
+      self.host.send_message_via_sms(message)
+    end
+  end
+
+  def host
+    @host = User.find(self.vacation_property[:user_id])
+  end
+
+  def guest
+    @guest = User.find_by(phone_number: self.guest_phone)
+  end
+
+  def confirm!
+    provision_phone_number
+    self.update!(status: "confirmed")
+  end
+
+  def reject!
+    self.update!(status: "rejected")
+  end
+
+  def notify_guest
+    if self.status_changed? && (self.status == "confirmed" || self.status == "rejected")
+      message = "Your recent request to stay at #{self.vacation_property.description} was #{self.status}."
+      self.guest.send_message_via_sms(message)
+    end
+  end
+
+  def send_message_to_guest(message)
+    message = "From #{self.host.name}: #{message}"
+    self.guest.send_message_via_sms(message, self.phone_number)
+  end
+
+  def send_message_to_host(message)
+    message = "From guest #{self.guest.name}: #{message}"
+    self.host.send_message_via_sms(message, self.phone_number)
+  end
+
+  private
+
+  def provision_phone_number
+    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+    begin
+      # Lookup numbers in host area code, if none than lookup from anywhere
+      @numbers = @client.account.available_phone_numbers.get('US').local.list(:area_code => self.host.area_code)
+      if @numbers.empty?
+        @numbers = @client.account.available_phone_numbers.get('US').local.list()
+      end
+
+      # Purchase the number & set the application_sid for voice and sms, will
+      # tell the number where to route calls/sms
+      @number = @numbers.first.phone_number
+      @anon_number = @client.account.incoming_phone_numbers.create(
+        :phone_number => @number,
+        :voice_application_sid => ENV['ANONYMOUS_APPLICATION_SID'], 
+        :sms_application_sid => ENV['ANONYMOUS_APPLICATION_SID']
+      )
+
+      # Set the reservation.phone_number
+      self.update!(phone_number: @number)
+
+    rescue Exception => e
+      puts "ERROR: #{e.message}"
+    end
+  end
+end
---
+++ anonymous-communications-rails/app/models/user.rb
@@ -0,0 +1,45 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :email,  presence: true, format: { with: /\A.+@.+$\Z/ }, uniqueness: true
+  validates :name, presence: true
+  validates :country_code, presence: true
+  validates :phone_number, presence: true, uniqueness: true
+  validates_length_of :password, in: 6..20, on: :create
+
+  has_many :vacation_properties
+  has_many :reservations, through: :vacation_properties
+
+  after_create :save_join_phone_number!
+
+  def send_message_via_sms(message, from_number = ENV['TWILIO_NUMBER'])
+    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+    @client.account.messages.create(
+      from: from_number,
+      to: self.phone_number,
+      body: message,
+    )
+  end
+
+  def check_for_reservations_pending
+    if pending_reservation
+      pending_reservation.notify_host(true)
+    end
+  end
+
+  def pending_reservation
+    self.reservations.where(status: "pending").first
+  end
+
+  def pending_reservations
+    self.reservations.where(status: "pending")
+  end
+
+  private
+
+  # No reason to save phone number without the area_code, it's what twilio & ActiveRecord expect
+  def save_join_phone_number!
+    self.update!(phone_number: "#{self.area_code}#{self.phone_number}")
+  end
+
+end
